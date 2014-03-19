43c4f395ca6bca94d170b9dfcbcef895b7b19b46
Joshua M. Clulow <jmc@joyent.com>
2014-03-19 11:44:02 -0700

    add auth support, clean up interface

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 /* vim: set ts=8 sts=8 sw=8 noet: */
 
+var mod_url = require('url');
 var mod_http = require('http');
 
 var mod_assert = require('assert-plus');
+var mod_verror = require('verror');
+
+var VError = mod_verror.VError;
 
 function
 TunnelClient(options)
 {
 	var self = this;
 
-	mod_assert.string(options.proxy_host, 'options.proxy_host');
-	mod_assert.number(options.proxy_port, 'options.proxy_port');
+	if (options.proxy_url) {
+		mod_assert.string(options.proxy_url, 'options.proxy_url');
+		var proxy_url = mod_url.parse(options.proxy_url);
+		mod_assert.strictEqual(options.proxy_url.protocol, 'http:',
+		    'unsupported proxy protocol');
 
-	self.hpt_proxy_host = options.proxy_host;
-	self.hpt_proxy_port = options.proxy_port;
+		self.hpt_proxy_host = proxy_url.hostname;
+		self.hpt_proxy_port = Number(proxy_url.port || 3128);
+		self.hpt_proxy_auth = proxy_url.auth;
+	} else {
+		mod_assert.string(options.proxy_host, 'options.proxy_host');
+		mod_assert.number(options.proxy_port, 'options.proxy_port');
+		mod_assert.optionalString(options.proxy_username,
+		    'options.proxy_username');
+		mod_assert.optionalString(options.proxy_password,
+		    'options.proxy_password');
+
+		self.hpt_proxy_host = options.proxy_host;
+		self.hpt_proxy_port = options.proxy_port;
+		if (options.proxy_username) {
+			self.hpt_proxy_auth = options.proxy_username + ':' +
+			     (options.proxy_password || '');
+		}
+	}
+
+	/*
+	 * At present, we only support a simple HTTP CONNECT-style
+	 * tunneling proxy.
+	 */
+	self.connect = http_tunnel_connect;
 
 	self.hpt_stats = {
 		hpts_ok: 0,
@@ -22,7 +51,13 @@ TunnelClient(options)
 }
 
 TunnelClient.prototype.connect = function
-connect(host, port, callback)
+connect()
+{
+	throw (new VError('run constructor'));
+};
+
+function
+http_tunnel_connect(host, port, callback)
 {
 	var self = this;
 
@@ -35,9 +70,10 @@ connect(host, port, callback)
 	var req_opts = {
 		host: self.hpt_proxy_host,
 		port: self.hpt_proxy_port,
+		auth: self.hpt_proxy_auth,
 		method: 'CONNECT',
 		path: host + ':' + port,
-		agent: false
+		agent: false,
 	};
 
 	var req = mod_http.request(req_opts);
@@ -47,9 +83,11 @@ connect(host, port, callback)
 			return;
 		done = true;
 
+		var _err = new VError(err, 'error while connecting to proxy');
+
 		req.removeAllListeners();
 		self.hpt_stats.hpts_fail++;
-		callback(err);
+		callback(_err);
 	});
 
 	req.on('response', function (res) {
@@ -57,11 +95,18 @@ connect(host, port, callback)
 			return;
 		done = true;
 
-		var err = new Error('server did not allow CONNECT (HTTP ' +
-		    res.statusCode + ')');
+		/*
+		 * Discard response body.
+		 */
+		res.resume();
+
+		var err = new VError('server did not allow CONNECT (HTTP %d)',
+		    res.statusCode);
 		err.statusCode = res.statusCode;
+		err.subresponse = res;
 
 		req.removeAllListeners();
+
 		self.hpt_stats.hpts_fail++;
 		callback(err);
 	});
@@ -71,27 +116,28 @@ connect(host, port, callback)
 			return;
 		done = true;
 
-		if (res.statusCode !== 200) {
-			var err = new Error('server did not allow CONNECT ' +
-			    '(HTTP ' + res.statusCode + ')');
-			err.statusCode = res.statusCode;
-			socket.destroy();
-			callback(err);
+		if (res.statusCode === 200) {
+			/*
+			 * Return the parsing overrun to the stream:
+			 */
+			socket.unshift(head);
+
+			req.removeAllListeners();
+			self.hpt_stats.hpts_ok++;
+			callback(null, socket);
 			return;
 		}
 
-		/*
-		 * Return the parsing overrun to the stream:
-		 */
-		socket.unshift(head);
-
-		req.removeAllListeners();
-		self.hpt_stats.hpts_ok++;
-		callback(null, socket);
+		var err = new Error('server did not allow CONNECT (HTTP %d)',
+		    res.statusCode);
+		err.statusCode = res.statusCode;
+		err.subresponse = res;
+		socket.destroy();
+		callback(err);
 	});
 
 	req.end();
-};
+}
 
 module.exports = {
 	TunnelClient: TunnelClient
